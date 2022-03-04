bd0dbbd9023a9e37a0537697571e3b5c6302c63b
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-03-04 15:25:55 +0800

    add search bar component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -6,6 +6,7 @@ export * from "./Dialog";
 export * from "./Dropdown";
 export * from "./Head";
 export * from "./Form";
+export * from "./SearchBar"
 
 // Composed
 export * from "./DropdownDialog";
