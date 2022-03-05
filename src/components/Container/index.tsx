965b88f901c363f9eef380aa372a5e17e2a600c6
TomasC <49659410+tx0c@users.noreply.github.com>
2022-03-05 06:41:44 +0000

    feat(mobile-hero): update mobile view

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import classNames from "classnames";
 
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="l-container">
-      <div className="l-row">
-        <div className="l-col-full">{children}</div>
-      </div>
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Container: React.FC<Props> = ({ children, className }) => (
+  <div className="l-container">
+    <div className="l-row">
+      <div className={classNames([className, "l-col-full"])}>{children}</div>
     </div>
-  );
-};
+  </div>
+);
--- src/views/Homepage/Hero/index.tsx
+++ src/views/Homepage/Hero/index.tsx
@@ -1,62 +1,63 @@
-// import Image from "next/image";
 import classNames from "classnames";
 
-import { Button } from "~/components";
+import { Button, Container, TextIcon } from "~/components";
 import { useResponsive } from "~/hooks";
 // import { BannerVideo } from "~/components/BannerVideo";
 
 import styles from "./styles.module.css";
 
 export const Hero = () => {
-  const isSmallUp = useResponsive("sm-up");
+  const isMediumUp = useResponsive("md-up");
 
   return (
     <section className={styles.hero}>
-      <section className={styles.intro}>
-        <h2>Deliver value and share benefits</h2>
-        <p className={styles.subtitle}>Create, Transfer, Relay</p>
-      </section>
+      <Container className={styles.content}>
+        <section className={styles.intro}>
+          <h2>Deliver value and share benefits</h2>
+          <p className={styles.subtitle}>Create, Transfer, Relay</p>
+        </section>
 
-      <section className={styles.dynamic}>
-        <div
-          style={{
-            minWidth: "340px",
-            minHeight: "340px",
-            backgroundImage: `url("/images/logbook-intro.png")`,
-          }}
-        ></div>
-        {/* <BannerVideo /> */}
-      </section>
+        <section className={styles.dynamic}>
+          {/* <div className={styles.image}></div> */}
+          <img src="/images/logbook-intro.png" width="100%" />
+          {/* <BannerVideo /> */}
+        </section>
 
-      <section className={styles.buttons}>
-        <Button
-          // width="15rem"
-          width={isSmallUp ? "15rem" : "100%"}
-          // spacing={["tight", "loose"]}
-          spacing={["tight", 0]}
-          textColor="blueGreen"
-          bgColor="lightMetal"
-          bgActiveColor="lightMetalHover"
-          // shadow={true}
-          borderRadius="1.5rem"
-          onClick={() => {}}
-        >
-          Claim
-        </Button>
-        <Button
-          width={isSmallUp ? "15rem" : "100%"}
-          // spacing={["tight", "loose"]}
-          spacing={["tight", 0]}
-          textColor="white"
-          bgColor="heavyMetal"
-          bgActiveColor="heavyMetalHover"
-          // shadow={true}
-          borderRadius="1.5rem"
-          onClick={() => {}}
-        >
-          My Bookcase
-        </Button>
-      </section>
+        <section className={styles.buttons}>
+          <Button
+            // width="15rem"
+            width={isMediumUp ? "15rem" : "100%"}
+            // spacing={["tight", "loose"]}
+            spacing={["tight", 0]}
+            // textColor="blueGreen"
+            bgColor="lightMetal"
+            bgActiveColor="lightMetalHover"
+            // shadow={true}
+            borderRadius="1.5rem"
+            onClick={() => {
+              // TODO: analytics
+            }}
+          >
+            <TextIcon color="blueGreen">Claim</TextIcon>
+          </Button>
+          <Button
+            width={isMediumUp ? "15rem" : "100%"}
+            // spacing={["tight", "loose"]}
+            spacing={["tight", 0]}
+            // textColor="white"
+            bgColor="heavyMetal"
+            bgActiveColor="heavyMetalHover"
+            // shadow={true}
+            borderRadius="1.5rem"
+            href="/bookcase"
+            onClick={() => {
+              // TODO: analytics
+            }}
+          >
+            <TextIcon color="white">My Bookcase</TextIcon>
+          </Button>
+        </section>
+      </Container>
 
       <section className={styles.leftRadial} />
       <section className={styles.rightRadial} />
