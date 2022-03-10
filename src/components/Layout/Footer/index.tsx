902e011d130da17eb785cb42961ef986cdf97335
TomasC <49659410+tx0c@users.noreply.github.com>
2022-03-10 17:17:49 +0000

    feat(): social-links template

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -6,10 +6,18 @@ import styles from "./styles.module.css";
 
 const Footer = () => (
   <footer className={classNames([styles.footer, "l-col-full"])}>
-    <a href={process.env.NEXT_PUBLIC_MATTERS_LINK} target="_blank" rel="noreferrer">
+    <a
+      href={process.env.NEXT_PUBLIC_TWITTER_LINK}
+      target="_blank"
+      rel="noreferrer"
+    >
       <TextIcon icon={<IconTwitter size="md" />} />
     </a>
-    <a href={process.env.NEXT_PUBLIC_DISCORD_LINK} target="_blank" rel="noreferrer">
+    <a
+      href={process.env.NEXT_PUBLIC_DISCORD_LINK}
+      target="_blank"
+      rel="noreferrer"
+    >
       <TextIcon icon={<IconDiscord size="md" />} />
     </a>
   </footer>
--- src/views/Homepage/Footer/index.tsx
+++ src/views/Homepage/Footer/index.tsx
@@ -28,17 +28,17 @@ const Footer = () => {
           <a href={process.env.NEXT_PUBLIC_OPENSEA_LINK} target="_blank">
             <TextIcon icon={<IconOpenSea size="mdS" />} />
           </a>
-          <a href={process.env.NEXT_PUBLIC_INSTAGRAM_LINK} target="_blank">
-            <TextIcon icon={<IconInstagram size="mdS" />} />
-          </a>
-          <a href={process.env.NEXT_PUBLIC_FACEBOOK_LINK} target="_blank">
-            <TextIcon icon={<IconFacebook size="mdS" />} />
+          <a href={process.env.NEXT_PUBLIC_DISCORD_LINK} target="_blank">
+            <TextIcon icon={<IconDiscord size="mdS" />} />
           </a>
           <a href={process.env.NEXT_PUBLIC_TWITTER_LINK} target="_blank">
             <TextIcon icon={<IconTwitter size="mdS" />} />
           </a>
-          <a href={process.env.NEXT_PUBLIC_DISCORD_LINK} target="_blank">
-            <TextIcon icon={<IconDiscord size="mdS" />} />
+          <a href={process.env.NEXT_PUBLIC_FACEBOOK_LINK} target="_blank">
+            <TextIcon icon={<IconFacebook size="mdS" />} />
+          </a>
+          <a href={process.env.NEXT_PUBLIC_INSTAGRAM_LINK} target="_blank">
+            <TextIcon icon={<IconInstagram size="mdS" />} />
           </a>
         </div>
       )}
@@ -55,20 +55,20 @@ const Footer = () => {
           <a href={process.env.NEXT_PUBLIC_OPENSEA_LINK} target="_blank">
             <div>Opensea</div>
           </a>
-          <a href={process.env.NEXT_PUBLIC_INSTAGRAM_LINK} target="_blank">
-            <div>Instagram</div>
-          </a>
-          <a href={process.env.NEXT_PUBLIC_FACEBOOK_LINK} target="_blank">
-            <div>Facebook</div>
+          <a href={process.env.NEXT_PUBLIC_DISCORD_LINK} target="_blank">
+            <div>Discord</div>
           </a>
           <a href={process.env.NEXT_PUBLIC_TWITTER_LINK} target="_blank">
             <div>Twitter</div>
           </a>
-          <a href={process.env.NEXT_PUBLIC_DISCORD_LINK} target="_blank">
-            <div>Discord</div>
+          <a href={process.env.NEXT_PUBLIC_FACEBOOK_LINK} target="_blank">
+            <div>Facebook</div>
           </a>
           <a href={process.env.NEXT_PUBLIC_TELEGRAM_LINK} target="_blank">
             <div>Telegram</div>
+          </a>
+          <a href={process.env.NEXT_PUBLIC_INSTAGRAM_LINK} target="_blank">
+            <div>Instagram</div>
           </a>
         </div>
       )}
