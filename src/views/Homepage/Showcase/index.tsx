8e9c1bf61393fdcafe5598d342876192f0a24ad6
TomasC <49659410+tx0c@users.noreply.github.com>
2022-03-18 00:50:25 +0000

    feat(): hide the showcase and move svg clip-path for intro-section

--- src/views/Homepage/Showcase/index.tsx
+++ src/views/Homepage/Showcase/index.tsx
@@ -159,22 +159,6 @@ export const Showcase = () => {
           }
         }
       `}</style>
-      <svg
-        className={styles.svg}
-        width="0"
-        height="0"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <clipPath
-            id="clipping"
-            clipPathUnits="objectBoundingBox"
-            transform="scale(0.005, 0.004385964912281)"
-          >
-            <path d="M0.030766 136.327C1.44292 77.7242 27.5678 64.3091 65.6961 24.7699C122.889 -22.5358 161.017 2.17614 188.554 62.897C216.091 123.618 194.202 199.872 120.064 222.466C45.9259 245.06 -1.38139 194.93 0.030766 136.327Z" />
-          </clipPath>
-        </defs>
-      </svg>
       <Container>
         <div className={styles.header}>
           <h1 className={styles.title}>Showcase</h1>
