054e696800d01fcd4d12ea8c0c98520c6b12ef25
TomasC <49659410+tx0c@users.noreply.github.com>
2022-03-21 16:48:50 +0000

    add google-site-verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ class LogbookDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />
+          <meta name="google-site-verification" content="lia3M50vaRwExj1rDvfv2C48aQ6-cnwxOhLZLex7rIo" />
         </Head>
 
         <body>
